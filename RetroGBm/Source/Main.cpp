823a282418ec1deef41da9e36c37284c2af8ab96
Callum <Callum5042@gmail.com>
2023-10-20 13:59:23 +0100

    Fixed reset bit 7 (#28)

--- RetroGBm/Source/Main.cpp
+++ RetroGBm/Source/Main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char** argv)
 	std::unique_ptr<Emulator> emulator = std::make_unique<Emulator>();
 	// emulator->LoadRom("Tetris.gb");
 	//emulator->LoadRom("D:\\Sources\\RetroGBm\\RetroGBm\\Resources\\testroms\\cpu_instrs\\individual\\01-special.gb");
-	emulator->LoadRom("D:\\Sources\\RetroGBm\\RetroGBm\\Resources\\testroms\\cpu_instrs\\individual\\09-op r,r.gb");
+	emulator->LoadRom("D:\\Sources\\RetroGBm\\RetroGBm\\Resources\\testroms\\cpu_instrs\\individual\\10-bit ops.gb");
 
 	// Message loop
 	try
