562a330a716b33d8a783878aa853e40bf137c2f8
Callum Anning <Callum5042@gmail.com>
2023-11-03 16:18:00 +0000

    Added WinMain entry point for windows system

--- RetroGBm/Source/Main.cpp
+++ RetroGBm/Source/Main.cpp
@@ -7,8 +7,25 @@
 // https://archive.org/details/GameBoyProgManVer1.1/page/n1/mode/2up
 // https://gekkio.fi/files/gb-docs/gbctr.pdf
 
+#ifdef _CONSOLE
+
 int main(int argc, char** argv)
 {
 	std::unique_ptr<Application> application = std::make_unique<Application>();
 	return application->Start();
 }
+
+#else
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
+{
+#ifdef _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	// _CrtSetBreakAlloc(972);
+#endif
+
+	std::unique_ptr<Application> application = std::make_unique<Application>();
+	return application->Start();
+}
+
+#endif // _CONSOLE
