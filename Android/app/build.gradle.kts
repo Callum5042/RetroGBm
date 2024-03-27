42c328cfe883e402cd24db9fd641c93794930b6b
Callum <Callum5042@gmail.com>
2024-03-27 13:10:20 +0000

    Updated android version number (#113)

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.4"
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
