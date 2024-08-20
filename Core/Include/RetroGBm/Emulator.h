2ec8fa93d08876de1e017c1c1ce5a6a9810257ab
Callum Anning <CallumAnning@hotmail.co.uk>
2024-08-20 10:27:12 +0100

    Updated byte alignment

--- Core/Include/RetroGBm/Emulator.h
+++ Core/Include/RetroGBm/Emulator.h
@@ -31,17 +31,19 @@ struct EmulatorContext
 	int cycles = 0;
 };
 
+#pragma pack(push, 1)
 struct SaveStateHeader
 {
 	char identifier[8] = { 'R', 'E', 'T', 'R', 'O', 'G', 'B', 'M' };
 	int version = 1;
 
-	time_t date_created = 0;
-	time_t date_modified = 0;
+    int64_t date_created = 0;
+	int64_t date_modified = 0;
 	double time_played = 0;
 
-	char reserved[24] = {};
+	char reserved[28] = {};
 };
+#pragma pack(pop)
 
 static_assert(sizeof(SaveStateHeader) == 64);
 
