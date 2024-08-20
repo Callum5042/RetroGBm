2c225310969244da639ccb54737ce667747219da
Callum Anning <CallumAnning@hotmail.co.uk>
2024-08-20 16:53:07 +0100

    Jetpack compose preview

--- Android/app/src/main/java/com/retrogbm/SaveStateActivity.kt
+++ Android/app/src/main/java/com/retrogbm/SaveStateActivity.kt
@@ -4,12 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class SaveStateActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
