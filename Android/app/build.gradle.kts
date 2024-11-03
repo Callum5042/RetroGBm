5a33a9473c64911dadbdd2a712fd936cba3dc041
Callum Anning <CallumAnning@hotmail.co.uk>
2024-11-03 22:29:02 +0000

    Updated Android to version v0.7.0

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.6.1"
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
