47ef94ab3f4536fa1cb611c1df6a77f6f1a4eb20
Callum Anning <CallumAnning@hotmail.co.uk>
2025-01-12 16:52:17 +0000

    Some working emulation

--- Android/app/src/main/java/com/retrogbm/EmulatorActivity.kt
+++ Android/app/src/main/java/com/retrogbm/EmulatorActivity.kt
@@ -1,7 +1,11 @@
 package com.retrogbm
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -36,31 +40,106 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.retrogbm.ui.theme.RetroGBmTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.nio.IntBuffer
 
 class EmulatorActivity : ComponentActivity() {
+
+    // Emulator components
+    private var emulator: EmulatorWrapper = EmulatorWrapper()
+
+    // Coroutines
+    private val emulatorCoroutineScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var emulatorThread: Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Load the ROM from the intent set by the home page
+        val title = intent.getStringExtra("ROM_TITLE")
+        if (!title.isNullOrEmpty()) {
+            val absolutePath = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.absolutePath
+            val path = absolutePath!! + "/ROMS/" + title
+            loadRom(Uri.fromFile(File(path)), title)
+        }
+
         setContent {
             RetroGBmTheme {
-                Content()
+                Content(emulator)
             }
+        }
+    }
+
+    private fun loadRom(uri: Uri?, fileName: String) {
+        val bytes = getRomBytes(uri!!)
+        val absolutePath = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.absolutePath
+
+        // Set battery path and possible create the folder
+        val batteryPath = absolutePath?.let { "$it/RomData" }!!
+        val batteryPathFolder = File(batteryPath)
+        if (!batteryPathFolder.exists()) {
+            batteryPathFolder.mkdirs()
+            Log.i("LoadROM", "Created folder $batteryPath")
+        }
+
+        // Load ROM
+        val batteryFilePath = batteryPath.let { "$it/$fileName.save" }
+        emulator.loadRom(bytes, batteryFilePath)
+
+        // Emulator background thread
+        emulatorThread = emulatorCoroutineScope.launch(Dispatchers.Default) {
+            while (emulator.isRunning()) {
+                emulator.tick()
+            }
+        }
+    }
+
+    private fun getRomBytes(uri: Uri): ByteArray {
+        val inputStream = contentResolver.openInputStream(uri)
+        val outputStream = ByteArrayOutputStream()
+        inputStream?.use { stream ->
+            val buffer = ByteArray(1024)
+            var length: Int
+
+            while (stream.read(buffer).also { length = it } != -1) {
+                outputStream.write(buffer, 0, length)
+            }
+        }
+
+        return outputStream.toByteArray()
+    }
+
+    companion object {
+        // Used to load the 'retrogbm' library on application startup.
+        init {
+            System.loadLibrary("retrogbm")
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Content() {
+fun Content(emulator: EmulatorWrapper) {
 
     val context = LocalContext.current
 
@@ -103,121 +182,56 @@ fun Content() {
             )
         },
     ) { innerPadding ->
-        EmulatorScreen(innerPadding)
+        EmulatorScreen(innerPadding, emulator)
     }
 }
 
 @Composable
-fun EmulatorScreen(innerPadding: PaddingValues) {
+fun EmulatorScreen(innerPadding: PaddingValues, emulator: EmulatorWrapper) {
     Box(
         modifier = Modifier.fillMaxHeight().padding(innerPadding)
     ) {
         Column {
-            Viewport()
-            Controls()
+            Viewport(emulator)
         }
     }
 }
 
 @Composable
-fun Controls() {
-    Column {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(IntrinsicSize.Max)
-        ) {
-            // D-Pad
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight() // Ensure the Box fills the height of the Row
-                    .padding(10.dp)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.button_dpad),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .aspectRatio(1f) // Ensures it stays square
-                        .align(Alignment.Center),
-                    contentScale = ContentScale.Fit
-                )
-            }
+fun Viewport(emulator: EmulatorWrapper) {
 
-            // A/B Buttons
-            Box(
-                modifier = Modifier
-                    .weight(1f) // Take up available space
-                    .fillMaxHeight() // Fill the vertical space
-            ) {
-                // A Button
-                Image(
-                    painter = painterResource(R.drawable.button_a),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .align(Alignment.Center) // Align A button to the left center
-                        .offset(x = 30.dp, y = (-15).dp),
-                    contentScale = ContentScale.Fit
-                )
+    // State to hold the Bitmap
+    val bitmapState = remember {
+        mutableStateOf(Bitmap.createBitmap(160, 144, Bitmap.Config.ARGB_8888))
+    }
 
-                // B Button
-                Image(
-                    painter = painterResource(R.drawable.button_b),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .align(Alignment.Center) // Align B button to the right center
-                        .offset(x = (-30).dp, y = 15.dp), // Offset to create spacing (half of button width)
-                    contentScale = ContentScale.Fit
-                )
-            }
-        }
+    // Launch a coroutine to update the bitmap in the background
+    LaunchedEffect(emulator) {
+        val updateTextureCoroutineScope = CoroutineScope(Dispatchers.Default)
+        updateTextureCoroutineScope.launch {
+            while (emulator.isRunning()) {
+                // Run the background work on a different thread
+                val pixels = withContext(Dispatchers.Default) {
+                    emulator.getVideoBuffer() // Get the video buffer on a background thread
+                }
 
-        // Start/Select Buttons
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Row(modifier = Modifier.align(Alignment.BottomCenter)) {
-                Image(
-                    painter = painterResource(R.drawable.startselect_button),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier.padding(10.dp).size(40.dp)
-                )
-                Image(
-                    painter = painterResource(R.drawable.startselect_button),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier.padding(10.dp).size(40.dp)
-                )
+                // Update the Bitmap on the main thread
+                withContext(Dispatchers.Main) {
+                    val updatedBitmap = Bitmap.createBitmap(160, 144, Bitmap.Config.ARGB_8888)
+                    val pixelsBuffer = IntBuffer.wrap(pixels.toList().toIntArray())
+                    updatedBitmap.copyPixelsFromBuffer(pixelsBuffer)
+                    
+                    bitmapState.value = updatedBitmap
+                }
             }
         }
     }
-}
 
-
-@Composable
-fun Viewport() {
     Image(
-        painter = painterResource(R.drawable.no_rom_loaded),
+        painter = BitmapPainter(bitmapState.value.asImageBitmap()),
         contentDescription = null,
         modifier = Modifier
             .fillMaxWidth(),
         contentScale = ContentScale.FillWidth
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun EmulatorScreenPreview() {
-    RetroGBmTheme {
-        Content()
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ControlScreenPreview() {
-    RetroGBmTheme {
-        Controls()
-    }
 }
