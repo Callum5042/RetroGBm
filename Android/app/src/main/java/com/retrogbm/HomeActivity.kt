fed8a83f7053e1b7e195a1cfdeab0e4e7ce2fa28
Callum Anning <CallumAnning@hotmail.co.uk>
2025-03-29 21:31:01 +0000

    Added Android menu icons

--- Android/app/src/main/java/com/retrogbm/HomeActivity.kt
+++ Android/app/src/main/java/com/retrogbm/HomeActivity.kt
@@ -20,9 +20,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.FileOpen
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -212,6 +214,7 @@ fun Content(previewRomData: ProfileRomData) {
                         ) {
                             DropdownMenuItem(
                                 text = { Text(stringResource(id = R.string.settings)) },
+                                leadingIcon = { Icon(Icons.Filled.Settings, null) },
                                 onClick = {
                                     showMenu = false
 
