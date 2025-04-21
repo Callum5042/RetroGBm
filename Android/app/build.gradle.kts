359906fb0714e2fdb07b6d340258ba8dd0781d58
Callum Anning <CallumAnning@hotmail.co.uk>
2025-04-21 13:07:42 +0100

    Updated version number

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "25.4.2"
+        versionName = "25.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
