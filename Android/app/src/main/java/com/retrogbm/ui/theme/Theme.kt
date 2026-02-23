b2b594419d7edcda04a2bc5ef050658b1a709acb
Callum Anning <CallumAnning@hotmail.co.uk>
2026-02-23 20:26:27 +0000

    Set system bar colours to black

--- Android/app/src/main/java/com/retrogbm/ui/theme/Theme.kt
+++ Android/app/src/main/java/com/retrogbm/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.retrogbm.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,16 +10,19 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color.White, // Set primary color to black
-    secondary = Color.Gray, // You can customize other colors as needed
+    primary = Color.White,
+    secondary = Color.Gray,
     tertiary = Color.DarkGray
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Color.Black, // Set primary color to black
+    primary = Color.Black,
     secondary = Color.Gray,
     tertiary = Color.LightGray
 )
@@ -39,6 +43,19 @@ fun RetroGBmTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
+    val context = LocalContext.current
+    val window = (context as Activity).window
+
+    // Window colour
+    window.statusBarColor = Color.Black.toArgb()
+    window.navigationBarColor = Color.Black.toArgb()
+
+    // Hide system bar
+//    WindowInsetsControllerCompat(window, window.decorView).apply {
+//        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//        hide(WindowInsetsCompat.Type.systemBars())
+//    }
 
     MaterialTheme(
         colorScheme = colorScheme,
